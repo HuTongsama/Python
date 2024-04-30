8f61c6f6ee9be2acbb2885ea112be881d0feda2f
HuTongsama <hutongsama@163.com>
2024-04-30 16:53:01 +0800

    update for menu_edit

--- TextEditor/textEditor.py
+++ TextEditor/textEditor.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 import tkinter.filedialog as fd
 from tkinter.messagebox import askyesno
 import os
@@ -6,14 +7,20 @@ class TextEditor:
     untitled = "Untitled"
     def __init__(self,master):
         self.save_path = ""
-
+        self.sel_start = tk.END
+        self.sel_end = tk.END
         self.window = master
         self.menuBar = tk.Menu(master=self.window)
         self.window.title(self.get_title(self.untitled))
         self.window.config(menu=self.menuBar)
         self.textBox = tk.Text(master=self.window)
         self.textBox.bind("<<Modified>>", self.on_content_changed)
-        self.textBox.grid()
+        self.textBox.bind("<<Selection>>", self.on_selection_changed)
+        ys = ttk.Scrollbar(self.window, orient = "vertical",
+                            command = self.textBox.yview)
+        self.textBox["yscrollcommand"] = ys.set
+        self.textBox.grid(row=0, column=0)
+        ys.grid(row=0, column=1,sticky="ns")
         self.menu_file = tk.Menu(master=self.menuBar,tearoff=False)
         self.menu_file.add_command(label="New",command=lambda:self.new_file())
         self.menu_file.add_command(label="New Window",command=lambda:self.new_window())
@@ -26,10 +33,10 @@ class TextEditor:
         self.menu_edit = tk.Menu(master=self.menuBar,tearoff=False)
         self.menu_edit.add_command(label="Undo")
         self.menu_edit.add_separator()
-        self.menu_edit.add_command(label="Cut")
-        self.menu_edit.add_command(label="Copy")
-        self.menu_edit.add_command(label="Paste")
-        self.menu_edit.add_command(label="Delete")
+        self.menu_edit.add_command(label="Cut",command=lambda:self.cut_text())
+        self.menu_edit.add_command(label="Copy",command=lambda:self.copy_text())
+        self.menu_edit.add_command(label="Paste",command=lambda:self.paste_text())
+        self.menu_edit.add_command(label="Delete",command=lambda:self.delete_text())
         self.menu_edit.add_separator()
         self.menu_edit.add_command(label="Find")
         self.menu_edit.add_command(label="Find Next")
@@ -110,11 +117,61 @@ class TextEditor:
         self.ask_save()
         self.window.destroy()
     
+    def undo(self):
+        pass
+
+    def cut_text(self):
+        self.copy_text()
+        self.delete_text()
+            
+    def copy_text(self):
+        if self.sel_start != tk.END:
+            text = self.textBox.get(self.sel_start,self.sel_end)
+            self.window.clipboard_clear()
+            self.window.clipboard_append(text)
+
+    def paste_text(self):
+        cursor_pos = self.textBox.index("insert")
+        text = self.window.clipboard_get()
+        self.textBox.insert(cursor_pos,text)
+    
+    def delete_text(self):
+        self.textBox.delete(self.sel_start,self.sel_end)
+
+    def find(self):
+        pass
+
+    def find_next(self):
+        pass
+
+    def find_previous(self):
+        pass
+
+    def replace(self):
+        pass
+
+    def goto(self):
+        pass
+
+    def select_all(self):
+        pass
+
     def on_content_changed(self,event):
         flag = self.textBox.edit_modified()
         if flag:
             old_title = self.window.title()
             self.window.title("*" + old_title)
         
-        
-t1 = TextEditor(tk.Tk())
+    def on_selection_changed(self,event):
+        sel_range = self.textBox.tag_ranges("sel")
+        if len(sel_range) > 0:
+            self.sel_start , self.sel_end = sel_range
+        else:
+            self.sel_start = tk.END
+            self.sel_end = tk.END
+
+def main():
+    t1 = TextEditor(tk.Tk())
+
+if __name__ == "__main__":
+    main()
